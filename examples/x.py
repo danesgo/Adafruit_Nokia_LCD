8029551b4c2c3148c9353bd115df8e6945712826
danesgo <degs432@hotmail.com>
2017-04-14 15:37:48 -0600

    added x.py example using initial buffer

---
+++ examples/x.py
@@ -0,0 +1,29 @@
+import time
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Hardware SPI usage:
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+
+# Initialize library.
+disp.begin(contrast=60)
+
+# Clear display.
+disp.clear()
+disp.display()
+
+print 'Press Ctrl-C to quit.'
+while True:
+	time.sleep(1.0)
