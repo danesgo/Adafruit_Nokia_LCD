9b5bb82c3771dedb83c41d5a9acbb06b6aedc71b
danesgo <degs432@hotmail.com>
2017-04-16 23:03:48 -0600

    added font files and text.py, just show message

---
+++ examples/text.py
@@ -0,0 +1,55 @@
+import time
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Hardware SPI usage:
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+
+# Initialize library.
+disp.begin(contrast=60)
+
+# Clear display.
+disp.clear()
+# display shows default buffer
+disp.display()
+
+time.sleep(5.0)
+#Clear display again
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white filled box to clear the image.
+draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+
+# Load font
+font = ImageFont.truetype('fipps.otf', 8)
+
+# Write some text.
+draw.text((8,30), 'Hello World!', font=font)
+
+# Display image.
+disp.image(image)
+disp.display()
+
+print 'Press Ctrl-C to quit.'
+while True:
+	time.sleep(1.0)
