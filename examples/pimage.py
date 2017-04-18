b4881b990c591d80a8b521dd6c4e9d3b53f6c462
danesgo <degs432@hotmail.com>
2017-04-17 21:41:56 -0600

    modified pimage.py example, now input = raw input

--- examples/pimage.py
+++ examples/pimage.py
@@ -18,20 +18,6 @@ SPI_DEVICE = 0
 # RST = 24
 # CS = 8
 
-# Beaglebone Black hardware SPI config:
-# DC = 'P9_15'
-# RST = 'P9_12'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
-
-# Beaglebone Black software SPI config:
-# DC = 'P9_15'
-# RST = 'P9_12'
-# SCLK = 'P8_7'
-# DIN = 'P8_9'
-# CS = 'P8_11'
-
-
 # Hardware SPI usage:
 disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
@@ -39,7 +25,10 @@ disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=40
 #disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
 
 # Initialize library.
-disp.begin(contrast=60)
+disp.begin(contrast=70)
+
+disp.display()
+time.sleep(2.0)
 
 # Clear display.
 disp.clear()
@@ -48,13 +37,12 @@ disp.display()
 # Load image and convert to 1 bit color.
 #image = Image.open('happycat_lcd.ppm').convert('1')
 
-# Alternatively load a different format image, resize it, and convert to 1 bit color.
-image = Image.open('nyan.png').resize((LCD.LCDWIDTH, LCD.LCDHEIGHT), Image.ANTIALIAS).convert('1')
-
-# Display image.
-disp.image(image)
-disp.display()
-
 print 'Press Ctrl-C to quit.'
 while True:
+	s = raw_input('Insert the image name, e.g. Bender.bmp')
+	# Alternatively load a different format image, resize it, and convert to 1 bit color.
+	image = Image.open(s).resize((LCD.LCDWIDTH, LCD.LCDHEIGHT), Image.ANTIALIAS).convert('1')
+	# Display image.
+	disp.image(image)
+	disp.display()
 	time.sleep(1.0)
